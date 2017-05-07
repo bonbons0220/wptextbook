4f42ce950946d03c2622990d3b9792fab8cc57af
bonbons0220 <bsouter@jhu.edu>
2017-05-07 10:41:19 -0400

    incremental broken

--- functions.php
+++ functions.php
@@ -12,17 +12,23 @@
 /**
  * Enqueue child theme style(s)
 **/
-function tb_enqueue_styles() {
+function wptb_enqueue_styles() {
+	
+	$min = ( defined( 'WP_ENV' ) && ( 'development' == WP_ENV ) ) ? '' : '.min' ;
+	
 	$parent_style = 'twentyfifteen-style';
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( $parent_style, 
+		get_template_directory_uri() . '/style.css' );
+    //wp_enqueue_style( 'bootstrap' , 
+		//get_stylesheet_directory_uri() . '/assets/bootstrap/css/bootstrap' . $min . '.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
 }
-add_action( 'wp_enqueue_scripts', 'tb_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'wptb_enqueue_styles' );
 
 // Custom Function to Include
 function my_favicon_link() {
@@ -39,7 +45,18 @@ add_action( 'wp_head', 'my_favicon_link' );
         //  Do something.
     //}
 //}
-
+/* Put the log in link on the main menu for now.
+function wptb_footer_hook( $name ) {
+	if ( defined( 'WP_ENV' ) && ( 'development' == WP_ENV ) ) {
+		if ( is_user_logged_in() ) {
+			echo '<div class="alert" role="alert"><a class="btn btn-sm btn-danger alignright" href="/wp-login.php?action=logout">Log Out</a><div>';					
+		} else {
+			echo '<div class="alert" role="alert"><a class="btn btn-sm btn-success alignright" href="/wp-login.php">Log In</a><div>';					
+		}
+	}
+}
+add_action( 'wp_footer', 'wptb_footer_hook', 8888 );
+*/
 /**
  * Include a file.
  *
